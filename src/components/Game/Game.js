0a69150ed45824886ef8e56c879186991337b424
Mihail Velikov <mihailvelik@gmail.com>
2023-03-08 22:23:00 +0200

    Exercise 1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,7 +9,30 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return Input();
 }
 
 export default Game;
+function Input() {
+  const [word, setWord] = useState('');
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(word);
+        setWord('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={word}
+        maxLength={5}
+        minLength={5}
+        onChange={({ target }) => setWord(target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
